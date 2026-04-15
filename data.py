e658b690e3b27254ff312cc2672f70794ccaae16
daspimenta <daspimenta@hotmail.com>
2026-04-14 23:14:50 -0300

    O código no arquivo helpers.py não deve ser modificado.

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-c5a4e348-ec65-4f5e-9e68-6cd0ff16e442.containerhub.tripleten-services.com?lng=pt' # Arquivo com constantes
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER	= '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,52 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def test_set_route(self):
+        #Adicionar em S8
+        pass
+        print("função criada para definir a rota")
+
+    def test_select_plan(self):
+        #Adicionar em S8
+        pass
+        print("função criada para definir o plano")
+
+    def test_fill_phone_number(self):
+        #Adicionar em S8
+        pass
+        print("função criada para preencher o número de telefone")
+
+    def test_fill_card(self):
+        #Adicionar em S8
+        pass
+        print("função criada para preencher o número do cartão")
+
+    def test_comment_for_driver(self):
+        #Adicionar em S8
+        pass
+        print("função criada para deixar mensagem para o motorista")
+
+    def test_order_blanket_and_handkerchiefs(self):
+        #Adicionar em S8
+        pass
+        print("função criada para pedir cobertor e guardanapos")
+
+    def test_order_2_ice_creams(self):
+        numbers_of_ice_creams = 2
+        for count in range(numbers_of_ice_creams):
+            #Adicionar em S8
+            print("função criada para pedir quantidade de sorvetes")
+            pass
+
+    def test_car_search_model_appears(self):
+        #Adicionar em S8
+        pass
+        print("função criada para buscar modelo do carro")
